f24f13e3af451c721b8036bceb105590d25eb40d
Parasuram Tanguturu <parasuram.tanguturu@siemens.com>
2019-09-06 11:23:23 +0530

    variables.tf and terraform.tfvars addded

--- network.tf
+++ network.tf
@@ -1,38 +1,38 @@
 resource "google_compute_network" "my_development_network" {
-  name = "devnetwork"
+  name                    = "devnetwork"
   auto_create_subnetworks = false
 }
 resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "${var.aws_ip_cidr_range}"
   enable_dns_hostnames = true
-  enable_dns_support = true
-  tags= {
+  enable_dns_support   = true
+  tags = {
     Name = "terraform-practice-aws-vpc"
   }
 }
 
 resource "azurerm_resource_group" "azy_network" {
   location = "South india"
-  name = "devresgrp"
-  tags ={
-    Name="terraform-practice-rg"
+  name     = "devresgrp"
+  tags = {
+    Name = "terraform-practice-rg"
   }
 }
 resource "azurerm_virtual_network" "blue_virtual_network" {
-  address_space = ["10.0.0.0/16"]
-  location = "South india"
-  name = "bluevirtnetwork"
+  address_space       = ["10.0.0.0/16"]
+  location            = "South india"
+  name                = "bluevirtnetwork"
   resource_group_name = "${azurerm_resource_group.azy_network.name}"
-  dns_servers = ["10.0.0.4","10.0.0.5"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
   subnet {
     address_prefix = "10.0.1.0/24"
-    name = "subnet1"
+    name           = "subnet1"
   }
   subnet {
     address_prefix = "10.0.2.0/24"
-    name = "subnet2"
+    name           = "subnet2"
   }
-  tags ={
-    Name="terraform-practice-rg"
+  tags = {
+    Name = "terraform-practice-rg"
   }
 }
